c0b759d75e273e0121427db4d4e1d5d246b0d570
Chris Piric <cpirdev2025@gmail.com>
2025-08-21 22:46:41 -0400

    Edit README.md and iloveloopy.py

--- iloveloopy.py
+++ iloveloopy.py
@@ -1,2 +1,9 @@
 for i in range(0, 10):
 	print("i: " + str(i) + " Loops are super cool!")
+
+print()
+
+for i in range(0, 10):
+	if i % 2 == 0:
+		print("i: " + str(i) + " Only even iterations please!")
+
